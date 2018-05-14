1b003da335c3007934f2c2325b31fdba0c549ac9
hultdin <34416082+hultdin@users.noreply.github.com>
2018-05-14 14:55:13 +0200

    Update signature-update.py

--- signature-update.py
+++ signature-update.py
@@ -2,7 +2,7 @@
 
 '''
 This python script generates SQL statements that can be used to update the signature table if the alert description 
-looks like "Snort Alert [1:1337:1]", i.e. there is no description matching the sid. 
+looks like "Snort Alert [1:1337:1]", i.e. there is no description matching the sid in the database. 
 
 grep 1337 /etc/suricata/sid-msg.map | signature-update.py
 '''
